db47082d1c84372b01cd761f3ea8ef2f6b781375
Frederick Johnson <fritzj96@gmail.com>
2021-07-12 15:12:36 -0500

    added: pragma to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+/**
+ * Filename: index.ts
+ * Last Editor: Frederick Johnson (FritzTheDev)
+ * Last Edited: 7/12/21
+ * Description: Entry point for the app that creates a database connection & starts the express server listening.
+ */
+
 import { Server } from './app';
 
-/** Bootstraps the app by connecting to the database & creating a server object that starts listening. */
+/** Bootstraps the app by connecting to the database & creating a server object, then starts it listening. */
 const main = () => {
   const app = new Server([]);
 
