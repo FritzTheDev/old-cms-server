5fb85f01431df8e8c6d36ebe948c5b4c76ba4eb2
Frederick Johnson <fritzj96@gmail.com>
2021-07-12 15:53:15 -0500

    formatting

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import cors from 'cors';
 import Express, { Application, json } from 'express';
 import { Controller } from './interfaces/controller-interface';
 import { errorHandlingMiddleware } from './middleware/error-handling-middleware';
+
 /** Wraps the express application, middleware, controllers, and error handling into one object.  */
 export class Server {
   private app: Application = Express();
@@ -21,7 +22,7 @@ export class Server {
   /** starts the app listening for requests on the port set by the PORT environment variable or 3000 if that's undefined. */
   public listen(): void {
     this.app.listen(process.env.PORT || 4200);
-    
+
     // eslint-disable-next-line no-console
     console.info(`Listening on ${process.env.PORT || 4200}`);
   }
