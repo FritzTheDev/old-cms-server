a3b8ee466750e106b250579aa56480e993a29f64
Frederick Johnson <fritzj96@gmail.com>
2021-07-12 15:10:06 -0500

    added: methods & documentation to app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,35 +4,45 @@
  * Last Edited: 7/12/21
  * Description: app.ts is where every part of the app (except database config) gets tied together & ready to start listening.
  */
-
+import CORS from 'cors';
 import Express, { Application, json } from 'express';
 import { Controller } from './interfaces/controller-interface';
 import { errorHandlingMiddleware } from './middleware/error-handling-middleware';
-
+/** Wraps the express application, middleware, controllers, and error handling into one object.  */
 export class Server {
   private app: Application = Express();
 
   constructor(controllers: Controller[]) {
-    this.configureMiddleware();
+    this.configureGlobalMiddleware();
     this.configureControllers(controllers);
     this.configureErrorHandling();
   }
 
+  /** starts the app listening for requests on the port set by the PORT environment variable or 3000 if that's undefined. */
   public listen(): void {
     this.app.listen(process.env.PORT || 3000);
   }
 
-  private configureMiddleware() {
+  /** Configures the global middleware that needs to be applied to every incoming request
+   * Includes:
+   * - Cross Origin Request Sharing via `cors`
+   * - JSON body parsing (bundled with Express but the API matches `body-parser`'s JSON method )
+   *
+   */
+  private configureGlobalMiddleware() {
+    // This checks if the app is running in prod. If it is, it requires the client's origin to be set.
+    this.app.use(CORS({ origin: process.env.NODE_ENV === 'production' ? process.env.CLIENT_ORIGIN : '*' }));
     this.app.use(json());
   }
 
-  /** Configures the express app to use  */
+  /** Configures the express app to use controllers that handle incoming requests. */
   private configureControllers(controllers: Controller[]) {
     controllers.forEach((controller) => {
       this.app.use(controller.path, controller.router);
     });
   }
 
+  /** Adds error handling middleware on a global basis. */
   private configureErrorHandling() {
     this.app.use(errorHandlingMiddleware);
   }
