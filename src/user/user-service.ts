502c353832a5804c916200fdc66a5aca0cdb03b7
Frederick Johnson <fritzj96@gmail.com>
2021-07-12 16:23:29 -0500

    added: further jsdoc-ing

--- src/user/user-service.ts
+++ src/user/user-service.ts
@@ -7,9 +7,12 @@
 import { User } from '@prisma/client';
 import { prisma } from '../database/prisma';
 
+/** Handles database queries & business logic - these instance methods get called from controllers. */
 export class UserService {
+  /** prisma instance that gives us access to a typed postgres db. Neat! */
   private db = prisma;
 
+  /** Returns a promise with an array of every user in the database */
   async getAllUsers(): Promise<User[]> {
     return this.db.user.findMany();
   }
