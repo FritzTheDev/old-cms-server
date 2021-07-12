4ea048be936dfa9a50377e33b337b5b5d58e0f56
Frederick Johnson <fritzj96@gmail.com>
2021-07-12 15:10:38 -0500

    added: jsdoc comment to main function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Server } from './app';
 
+/** Bootstraps the app by connecting to the database & creating a server object that starts listening. */
 const main = () => {
   const app = new Server([]);
 
