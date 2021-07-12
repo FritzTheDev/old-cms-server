b90b1737fa27d4b387726c4aa19c2d251acb78e2
Frederick Johnson <fritzj96@gmail.com>
2021-07-12 15:29:44 -0500

    added: lastedited filedoc

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,3 +1,10 @@
+/**
+ * Filename: database/prisma.ts
+ * Last Editor: Frederick Johnson (FritzTheDev)
+ * Last Edited: 7/12/21
+ * Description: Exports a prisma object instance that allows me to run db queries.
+ */
 import { PrismaClient } from '@prisma/client';
 
+/** Prisma client instance - gives me access to everything I'll need from my DB. */
 export const prisma = new PrismaClient();
