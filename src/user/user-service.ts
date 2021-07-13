e956d6d1228f33d8d9aaab1c2f497f0928d00375
Frederick Johnson <fritzj96@gmail.com>
2021-07-12 22:34:16 -0500

    updated: user service to include a createUser method

--- src/user/user-service.ts
+++ src/user/user-service.ts
@@ -1,11 +1,12 @@
 /**
- * Filename: user-service.ts
+ * Filename: user/user-service.ts
  * Last Editor: Frederick Johnson (FritzTheDev)
  * Last Edited: 7/12/21
  * Description: Handles database queries & business logic, primarily from the user controller.
  */
 import { User } from '@prisma/client';
 import { prisma } from '../database/prisma';
+import { CreateUserDTO } from './user-dtos';
 
 /** Handles database queries & business logic - these instance methods get called from controllers. */
 export class UserService {
@@ -20,5 +21,10 @@ export class UserService {
   /** Returns a promise with an array of a single user that matches the given ID */
   async getOneUser(id: number): Promise<User> {
     return this.db.user.findUnique({ where: { id } });
+  }
+
+  /** Creates a user & returns a promise with that user */
+  async createUser(data: CreateUserDTO): Promise<User> {
+    return this.db.user.create({ data })
   }
 }
