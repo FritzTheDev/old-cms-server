3a606b6a74b9c4c80afc9cbd091f801038f09791
Frederick Johnson <fritzj96@gmail.com>
2021-07-15 00:05:18 -0500

    cleanup & prisma post schema removed

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -4,7 +4,7 @@ import { HttpException } from '../exceptions/http-exception';
 /** Prisma client instance - gives me access to everything I'll need from my DB. */
 export const prisma = new PrismaClient({
   rejectOnNotFound: {
-    findFirst: { User: () => new HttpException(404, 'User Not Found'), Post: () => new HttpException(404, 'Post Not Found') },
-    findUnique: { User: () => new HttpException(404, 'User Not Found'), Post: () => new HttpException(404, 'Post Not Found') },
+    findFirst: { User: () => new HttpException(404, 'User Not Found') },
+    findUnique: { User: () => new HttpException(404, 'User Not Found') },
   },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import "./auth/jwt-strategy";
 import { Server } from './app';
 import { AuthController } from './auth/auth-controller';
 import { UserController } from './user/user-controller';
